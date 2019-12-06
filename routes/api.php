daeb232042aadbbc15153ec33785a41d0b87972f
hoseiny <ahoseiny@trudoc24x7.com>
2019-12-06 17:21:33 +0200

    init infyom

--- routes/api.php
+++ routes/api.php
@@ -2,21 +2,17 @@
 
 use Illuminate\Http\Request;
 
-Route::group(['prefix' => 'api'], function () {
-    Route::group(['prefix' => 'v1'], function () {
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
 
-        //Auth
-        Route::group(['middleware' => 'api', 'prefix' => 'auth'], function ($router) {
-            Route::post('login', 'AuthController@login');
-            Route::post('logout', 'AuthController@logout');
-            Route::post('refresh', 'AuthController@refresh');
-            Route::post('me', 'AuthController@me');
-        });
-
-
-
-
-
-    });
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
 });
-
